a957eedd718a78b7daee23e86627de60e8731d90
Valentina <88043734+Pezcue@users.noreply.github.com>
2024-02-22 19:34:18 -0500

    Update HtmlValidator.java

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -1,16 +1,32 @@
 import java.util.Queue;
 import java.util.Stack;
 
-
 public class HtmlValidator {
 
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
-
 		/* IMPLEMENT THIS METHOD! */
 
-		return null; // this line is here only so this code will compile if you don't modify it
+		Stack<HtmlTag> pilaAperturas = new Stack<>();
+
+		for (HtmlTag etiqueta : tags) {
+			if (etiqueta.isSelfClosing()) {
+				continue;
+			} else if (etiqueta.isOpenTag()) {
+				pilaAperturas.push(etiqueta);
+			} else if (!etiqueta.isOpenTag()) {
+				if (!pilaAperturas.isEmpty()) {
+					if (pilaAperturas.peek().matches(etiqueta)) {
+						pilaAperturas.pop();
+					} else {
+						return pilaAperturas;
+					}
+				} else {
+					return null;
+				}
+			}
+
+		}
+		return pilaAperturas; // this line is here only so this code will compile if you don't modify it
 	}
-	
 
 }
-
